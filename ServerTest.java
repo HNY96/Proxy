2c4be6f88ded0a9c1b5a8c8f77fdcd5256555c26
HNY96 <pluton.he@gmail.com>
2016-11-26 21:12:55 +0800

    Add files via upload
    function 1 is almost completed, and function 2 is completed but not fancy.

--- ServerTest.java
+++ ServerTest.java
@@ -10,10 +10,19 @@ import java.util.zip.ZipFile;
  */
 public class ServerTest {
     public static void main(String[] args) {
+
         ServerSocket serverSocket = null;
         Socket sock = null;
 
         try {
+            //建一个文件写入流，向其中写入响应和请求信息
+            File writename = new File("D:\\java_project\\src\\proxy\\request.txt");
+            writename.createNewFile();
+            BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+            File writeresponse = new File("D:\\java_project\\src\\proxy\\response.txt");
+            writeresponse.createNewFile();
+            BufferedWriter outresponse = new BufferedWriter(new FileWriter(writeresponse));
+
             //建立一个浏览器到代理的socket
             serverSocket = new ServerSocket(1234);
             sock = serverSocket.accept();
@@ -25,13 +34,15 @@ public class ServerTest {
             String message = "";
             boolean isFinish = false;
             while (!isFinish && (line = reader.readLine()) != null) {
-                line += "\n";
+                line += "\r\n";
                 message += line;
-                if (line.equals("\n")) {
-//                    message = message.substring(0, message.length()-2);
+                if (line.equals("\r\n")) {
                     isFinish = true;
                 }
             }
+            out.write(message);
+            out.flush();
+            out.close();
             System.out.print(message);
 
             //提取url
@@ -47,51 +58,42 @@ public class ServerTest {
             BufferedReader reader1 = new BufferedReader(stream1);
 
             //将请求报文切开，发送请求到指定主机
-            int i = message.split("\n").length;
+            int i = message.split("\r\n").length;
             for (int j = 0; j < i; j++) {
-                String temp = message.split("\n")[j];
-                writer.println(temp);
+                String temp = message.split("\r\n")[j];
+                if (temp.contains("Accept-Encoding")) {//必须要这句话，把压缩格式去掉，省掉解压缩的烦恼
+                    continue;
+                }
+                writer.print(temp+"\r\n");
             }
-            writer.println("Connection: Close");//大坑！！！！！
-            writer.println();
+            writer.println("Connection: Close");//大坑！！！！！如果没有这句话对面不会把消息flush，因此收不到响应
+            writer.print("\r\n");
+            writer.flush();
 
             //线程关闭两秒，等待服务器响应，拿到服务器返回的响应报头以及html文件
             String line1;
             String message1 = "";
-            Thread.sleep(2000);
+            Thread.sleep(1000);
             if (reader1.ready()) {
                 while ((line1 = reader1.readLine()) != null) {
-                    line1 += "\n";
+                    line1 += "\r\n";
                     message1 += line1;
                 }
             }
             System.out.println(message1);
+            outresponse.write(message1);
+            outresponse.flush();
+            outresponse.close();
 
             //将html文件和响应报头发送给浏览器
             PrintWriter toBrowser = new PrintWriter(sock.getOutputStream());
-//            i = message1.split("\n").length;
-//            for (int j = 0; j < i; j++) {
-//                String temp = message1.split("\n")[j];
-//                toBrowser.println(temp);
-//            }
-            toBrowser.print("HTTP/1.1 200 OK\r\n");
-//            toBrowser.println("Cache-Control: no-cache");
-//            toBrowser.println("Pragma: no-cache");
-//            toBrowser.println("Content-Type: text/html; charset=utf-8");
-//            toBrowser.println("Content-Encoding:");
-//            toBrowser.println("Expires: -1");
-//            toBrowser.println("Vary: Accept-Encoding");
-//            toBrowser.println("Server: Microsoft-IIS/8.5");
-//            toBrowser.println("X-AspNetMvc-Version: 5.1");
-//            toBrowser.println("X-AspNet-Version: 4.0.30319");
-//            toBrowser.println("X-Powered-By: ASP.NET");
-//            toBrowser.println("Date: Fri, 25 Nov 2016 12:32:11 GMT");
-//            toBrowser.println("Connection: close");
-//            toBrowser.println("Content-Length: 0");
-            toBrowser.print("Content-Type: text/html\r\n");
-            toBrowser.println();
-            toBrowser.print("Hello");
-            toBrowser.println();
+            i = message1.split("\r\n").length;
+            for (int j = 0; j < i; j++) {
+                String temp = message1.split("\r\n")[j];
+                toBrowser.println(temp);
+            }
+            toBrowser.flush();
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
