1c9a0337f6c520d8bb627527d3dd128c1d7c763f
zbh159173 <979713385@qq.com>
2016-12-01 23:59:29 +0800

    Add files via upload
    增加了文件写入功能，修复了部分bug

--- RequestThread.java
+++ RequestThread.java
@@ -1,9 +1,7 @@
 package proxy;
 
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
 import java.util.HashMap;
@@ -35,12 +33,15 @@ public class RequestThread implements Runnable {
     public void clientToProxy() throws IOException {
         StringBuilder request = new StringBuilder();
 
+        File writename = new File("C:\\Users\\zbh\\Desktop\\request.txt");
+        writename.createNewFile();
+        BufferedWriter outrequest = new BufferedWriter(new FileWriter(writename));
+
         int tempReader;
         int lineNumber = 0;
         int isEndOfRequest = 0;
         //String requestMethod = new String();
         String fileURL = new String();
-        URL url = new URL(fileURL);
         //String HttpVersion = new String();
         String key = new String();
         String value = new String();
@@ -53,26 +54,34 @@ public class RequestThread implements Runnable {
                     String requestHeadline = request.toString();
                     lineNumber += 1;
                     if (lineNumber == 1) {
+                        outrequest.write(requestHeadline + "\r\n");
                         requestMethod = requestHeadline.split(" ")[0];
                         if (!requestMethod.toLowerCase().equals("get") && !requestMethod.toLowerCase().equals("post")){
                             requestMethod = null;
                             break;
                         }
-                        fileURL = requestHeadline.split(" ")[1].toLowerCase();
+                        fileURL = requestHeadline.split(" ")[1];
+                        URL url = new URL(fileURL);
                         url_for_sending = url.getFile();
                         HttpVersion = requestHeadline.split(" ")[2];
                     }
                     if (lineNumber > 1) {
                         key = requestHeadline.split(":")[0].toLowerCase();
                         value = requestHeadline.split(":")[1].toLowerCase();
+                        if(!key.equals("cookie") && !key.equals("accept-encoding") &&
+                                !key.equals("proxy-connection") && !key.equals("user-agent")){
+                            outrequest.write(requestHeadline + "\r\n");
+                        }
                         header.put(key, value);
                         if(header.containsKey("cookie")) header.remove("cookie");
                         if(header.containsKey("accept-encoding")) header.remove("accept-encoding");
                         if(header.containsKey("proxy-connection")) header.remove("proxy-connection");
+                        if(header.containsKey("user-agent")) header.remove("user-agent");
                     }
                     request.delete(0, request.length());
                 }
                 if (isEndOfRequest == 3){
+                    outrequest.write("\r\n");
                     break;
                 }
             } else {
@@ -86,27 +95,32 @@ public class RequestThread implements Runnable {
             while ((tempReader = clientInput.read()) != -1){
                 if ((char) (tempReader) == '\r' || (char) (tempReader) == '\n') {
                     flag += 1;
+                    request.append((char) tempReader);
                 }
                 else {
                     flag = 0;
                     request.append((char) tempReader);
                 }
-                if (flag == 3) {
+                if (flag == 4) {
                     requestHeadline_2 = request.toString();
+                    outrequest.write(requestHeadline_2);
                     break;
                 }
             }
         }
 
-        /*System.out.println(requestMethod + " " + fileURL + " " + HttpVersion);
+        outrequest.flush();
+        outrequest.close();
+
+        System.out.println(requestMethod + " " + fileURL + " " + HttpVersion);
         for (Map.Entry<String, String> entry : header.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-        System.out.println(requestHeadline_2);*/                                     //测试代码
+        System.out.println(requestHeadline_2);                                     //测试代码
 
-        while ((tempReader = clientInput.read()) != -1) {
+        /*while ((tempReader = clientInput.read()) != -1) {
             bos.write(tempReader);
-        }
+        }*/
     }
 
     public void ProxyToServer() {
